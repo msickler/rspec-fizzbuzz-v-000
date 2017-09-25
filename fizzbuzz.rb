2a0bce7f8b9a7729c5cba570e10adda19361bd4c
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-25 22:03:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
-def fizzbuzz
+def fizzbuzz(int)
+  if int % 3 == true
+    "Fizz"
+  end
+else int % 5 == true
+  "Buzz"
+end
+elsif (int % 3) && (int % 5) == true
+  "Fizzbuzz"
+end
+elsif !((int % 3) && (int % 5))
+  ""
+end
 end
